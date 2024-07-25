543d1be674c4c1138721529975f3107c42902496
Grispys <grispanimation@gmail.com>
2024-07-25 12:25:26 -0230

    added comments

--- src/components/JankSolution.jsx
+++ src/components/JankSolution.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Doggs from './Doggs';
 import ImageGallery from './ImageGallery';
 
+
+// I wanted to pass variables betweeen these two components and couldn't find a good solution, so i made a "parent" component so to speak, that contained both components, and when a
+// variable was changed in one, it would send to the parent component, and update any other component's definition of said variable.
 const JankSolution = () => {
   const [selectedID, setSelectedID,] = useState('');
   const [numImg, setNumImg] = useState(1);
