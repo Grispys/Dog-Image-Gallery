4d054b68fd8e93b3d8cb9d6f9f1f64e1af2ed7d5
Grispys <grispanimation@gmail.com>
2024-08-01 19:02:37 -0230

    final commit

--- src/components/JankSolution.jsx
+++ src/components/JankSolution.jsx
@@ -6,8 +6,8 @@ import ImageGallery from './ImageGallery';
 // I wanted to pass variables betweeen these two components and couldn't find a good solution, so i made a "parent" component so to speak, that contained both components, and when a
 // variable was changed in one, it would send to the parent component, and update any other component's definition of said variable.
 const JankSolution = () => {
-  const [selectedID, setSelectedID,] = useState('');
-  const [numImg, setNumImg] = useState(1);
+  const [selectedID, setSelectedID,] = useState(''); // ID variables hold string data as it relates to the dog breeds
+  const [numImg, setNumImg] = useState(1); // numImg uses integer usestate because they contain the whole number that represents the needed amount of dogs
 
   return (
     <div>
