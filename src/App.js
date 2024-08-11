d2629ebdd34f4631339c21f47eb8d291dc3ca690
Grispys <grispanimation@gmail.com>
2024-08-10 22:51:31 -0230

    added extra comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <p>Welcome to Canines & Other Delights. <img src={image} alt='doggy' height={50} width={50} title='Thanks to dog.ceo and Stanford Dogs for the API dataset.'/></p> 
       <header className="App-header">
+        {/* unfortunately, react-sound was causing some errors when it was not installed on end-user terminals. For simplicity it has been removed. */}
       <JankSolution/>
       </header>
       <p>Remember to allow this website to "autoplay audio" for the full experience.</p>
